b84e9c4784a3ce3c43031a23b72e58266527906c
Teddy Belitsos <tbelitsos@gmail.com>
2020-07-01 23:25:54 -0500

    Add feedback from pull request https://github.com/teddyb2/librecon/pull/1
    Reviewed pull request reviews and implemented feedback.
    Enhanced error handling to detect invalid and blank domains.

--- domain_recon.py
+++ domain_recon.py
@@ -1,76 +1,77 @@
-#This is the domain recon module for librecon
-#The purpose of this module is to parse an dump of apache's vhost
-#for the top level domain "TLD" of the domain of interest.
+# This is the domain recon module for librecon
+# The purpose of this module is to parse an dump of apache's vhost
+# for the top level domain 'TLD' of the domain of interest.
 
 import sys
 
 from utils import do_cmd as bash_cmd
-#to-do implement error handeling for bad input, i.e no domain or domain that doesn't exist.
-#The function returns two strings, ret contains the error code, 0 or 1 and the second string
-#contains the information that the bash command returns
+
+
+# TODO: implement error handeling for bad input, i.e no domain or domain that doesn't exist.
+# The function returns two strings, ret contains the error code, 0 or 1 and the second string
+# contains the information that the bash command returns
 def get_domain_config_path(domain):
-    ret, vhost = bash_cmd(f'httpd -S | grep {domain}')
+    ret, vhost = bash_cmd(f'httpd -S | grep "{domain}"')
     if ret == 1: 
-        print(f"Domain: {domain} not found, exiting.")
+        print(f'Domain: {domain} not found, exiting.')
         sys.exit(1)
-    elif ret == 2:
-        print(f"No TLD provided, exiting.")
+    elif len(domain) == 0:
+        print(f'No TLD provided, exiting.')
         sys.exit(1)
-    return vhost.split('(')[-1].split(':')[0]
-
-#testing, domain = "teamrocket.org"
-domain = input("Initializing Domain Recon.. Please enter the TLD to recon: ")
+    domain_config_path = vhost.split('(')[-1].split(':')[0]
+    return domain_config_path
 
 
-#extracts the absolute path of the vhost config for the specified TLD
+# testing, domain = 'teamrocket.org'
+domain = input('Initializing Domain Recon.. Please enter the TLD to recon: ')
+
+# extracts the absolute path of the vhost config for the specified TLD
 extracted_path = get_domain_config_path(domain)
 
-ret, domain_info = bash_cmd(f'grep -i "document\|log" {extracted_path}')
+ret, domain_info = bash_cmd(f'grep -i \'document\|log\' {extracted_path}')
 
-#created an empty list to store strings that contains cleaned up output
-#from the apache vhost dump
+# created an empty list to store strings that contains cleaned up output
+# from the apache vhost dump
 clean_domain_info = list()
 
-#iterating over domain_info, splitting into substrings on \n
+# iterating over domain_info, splitting into substrings on \n
 for item in domain_info.split('\n'):
-    #itermediate variable to strip item of whitespace
+    # itermediate variable to strip item of whitespace
     clean_item = item.strip()
-    #if the string length of clean_item > 0, append the substring to the 
-    #clean_domain_info variable 
+    # if the string length of clean_item > 0, append the substring to the 
+    # clean_domain_info variable 
     if len(clean_item) > 0:
         clean_domain_info.append(clean_item)
 
-#list comprehension
-item = [item.strip() for item in domain_info.split('\n') if len(item) > 0]
 
-#item is an list that contains the following:
-#-documentroot
-#-custom/access logs
-#-error logs
+# item is an list that contains the following:
+# -document_root
+# -custom/access logs
+# -error logs
 
-#to-do: create an dictionary that contains key value pairs of httpd directives to config locations absolute
-vhost_info = {}
+# TODO: create an dictionary that contains key value pairs of httpd directives to config locations absolute
 
-documentroot = "DocumentRoot"
-customlog = "CustomLog"
-errorlog = "ErrorLog"
+DOCUMENT_ROOT_MARKER = 'DocumentRoot'
+CUSTOM_LOG_MARKER = 'CustomLog'
+ERROR_LOG_MARKER = 'ErrorLog'
 
-#info is a string split into two strings: keyword and /path/to/some/log
-for line in item:
-    if documentroot in line:
+
+# info is a string split on an whitespace character into two strings: keyword and /path/to/some/log
+for line in clean_domain_info:
+    if DOCUMENT_ROOT_MARKER in line:
         info = line.split(' ')
         document_root_path = info[1]
-    elif customlog in line:
+    elif CUSTOM_LOG_MARKER in line:
         info = line.split(' ')
         custom_log_path = info[1]
-    elif errorlog in line:
+    elif ERROR_LOG_MARKER in line:
         info = line.split(' ')
         error_log_path = info[1]
 
-#This section returns the basic information above the domain to the user
+# This section returns the basic information above the domain to the user
 print(f'\n')
 print(f'The following information was reconned:')
 print(f'Vhost Configuration: {extracted_path}') 
-print(f'{documentroot}: {document_root_path}')
-print(f'{customlog}: {custom_log_path}')
-print(f'{errorlog}: {error_log_path}')
+print(f'{DOCUMENT_ROOT_MARKER}: {document_root_path}')
+print(f'{CUSTOM_LOG_MARKER}: {custom_log_path}')
+print(f'{ERROR_LOG_MARKER}: {error_log_path}')
